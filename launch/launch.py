57d7dc4c19d706d777130fd11acc2d69cfcfbfd9
Dangield <dan.gield@gmail.com>
2019-12-17 18:39:02 +0100

    working on launch script, working temperature node sending sensor_msgs/Temperature type messages, using .yaml files for parameters

---
+++ launch/launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_prefix
+import launch_ros.actions
+package = "minirys_drivers"
+
+
+def generate_launch_description():
+	path = get_package_prefix(package)
+	path_list = path.split('/')
+	del path_list[-2]
+	path_list.insert(-1, 'src')
+	full_path = '/'.join(path_list)
+
+	return LaunchDescription([
+		launch_ros.actions.Node(
+			# the name of the executable is set in CMakeLists.txt, towards the end of
+			# the file, in add_executable(...) and the directives following it
+			package= package, node_executable='temperature_publisher.py', output='screen',
+			parameters=[full_path + "/yaml/temperature_publisher_params.yaml"]),
+
+	])
---
+++ scripts/temperature_publisher.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+from tmp102 import TMP102
+import rclpy
+from rclpy.node import Node
+from rclpy.clock import Clock
+from std_msgs.msg import Header
+from sensor_msgs.msg import Temperature
+
+class TmpPublisher(Node):
+
+	def __init__(self):
+		super().__init__('temperature_publisher')
+
+		period = self.declare_parameter("period")
+		self.timer_period = period.value
+		units = self.declare_parameter("units")
+		self.tmp_units = units.value
+
+		self.publisher_ = self.create_publisher(Temperature, 'tmp', 10)
+		self.tmp = TMP102('C', 0x48, 1)
+		self.tmp.setUnits(self.tmp_units)
+
+		self.timer = self.create_timer(self.timer_period, self.timer_callback)
+
+	def timer_callback(self):
+		msg = Temperature()
+		h = Header()
+
+		# uncomment for SYSTEM_TIME
+		h.stamp = Clock().now().to_msg()
+
+		# uncomment for ROS_TIME
+		# h.stamp = self.get_clock().now().to_msg()
+
+		msg.header = h
+		msg.temperature = self.tmp.readTemperature()
+		msg.variance = 0.0
+		self.publisher_.publish(msg)
+
+if __name__ == '__main__':
+	rclpy.init()
+	tmp_publisher = TmpPublisher()
+	rclpy.spin(tmp_publisher)
+	tmp_publisher.destroy_node()
+	rclpy.shutdown()
