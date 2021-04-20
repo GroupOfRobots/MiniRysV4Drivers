1821dee8fec0c3ce2261065fb56a67b635322dcf
Dangield <dan.gield@gmail.com>
2021-04-20 20:34:19 +0200

    rewritten odometry node but something is wrong

--- include/nodes/OdometryCalculatorNode.hpp
+++ include/nodes/OdometryCalculatorNode.hpp
@@ -1,25 +1,33 @@
 #include "rclcpp/rclcpp.hpp"
 #include "../data_structures.h"
 #include "../FrequencyCounter/FrequencyCounter.hpp"
+#include "minirys_interfaces/msg/motors_controller_output.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+
+#define STEPS_TO_REVS 2*M_PI/200 // muliply by wheel radius to receive distance
 
 class OdometryCalculatorNode : public rclcpp::Node{
 	public:
-		OdometryCalculatorNode(motor_data&, robot_control_data&, odometry_data&);
+		OdometryCalculatorNode();
+		~OdometryCalculatorNode();
 	private:
-		motor_data *motorDataStructure;
-		robot_control_data *robotControlDataStructure;
-		odometry_data *odometryDataStructure;
 		float period;
-		float position[3];
+		float x, y, angle;
 		float wheelDistance, wheelRadius_l, wheelRadius_r;
-		float leftSpeed, previousLeftSpeed;
-		float rightSpeed, previousRightSpeed;
+		float speed_l, setSpeed_l, previousSetSpeed_l, speed_r, setSpeed_r, previousSetSpeed_r;
 		float acceleration;
-		bool printRobotLocation;
-		FrequencyCounter counter;
-		rclcpp::TimerBase::SharedPtr odometry_calculation_timer;
+		int64_t currentTime, lastMessageTime;
+		float phaseDuration;
+		float accelerationTime_l, accelerationTime_r, distance_l, distance_r;
 
-		void calculatePosition();
+		FrequencyCounter *counter;
+		rclcpp::TimerBase::SharedPtr odometry_calculation_timer;
+		rclcpp::Subscription<minirys_interfaces::msg::MotorsControllerOutput>::SharedPtr motors_controller_subscriber;
+		rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odometry_publisher;
+		nav_msgs::msg::Odometry msg;
+
+		void receiveCurrentSetSpeeds(const minirys_interfaces::msg::MotorsControllerOutput::SharedPtr msg);
+		void updatePosition();
 		void printLocation();
 		void cropAngle();
 		void setPosition(float, float, float);
--- src/nodes/OdometryCalculatorNode.cpp
+++ src/nodes/OdometryCalculatorNode.cpp
@@ -1,33 +1,29 @@
 #include "nodes/OdometryCalculatorNode.hpp"
+#include <cmath>
+#include <algorithm>
 
-OdometryCalculatorNode::OdometryCalculatorNode(motor_data& motorStructure, robot_control_data& robotControlStructure, odometry_data& odometryStructure): Node("odometry_calculator") {
-	motorDataStructure = &motorStructure;
-	robotControlDataStructure = &robotControlStructure;
-	odometryDataStructure = &odometryStructure;
+OdometryCalculatorNode::OdometryCalculatorNode(): Node("odometry_calculator") {
+	counter = new FrequencyCounter("odometry");
 
 	// initialize robot position
 	this->declare_parameter("initial_x", rclcpp::ParameterValue(0.0));
 	this->declare_parameter("initial_y", rclcpp::ParameterValue(0.0));
 	this->declare_parameter("initial_theta", rclcpp::ParameterValue(0.0));
-	position[0] = this->get_parameter("initial_x").get_value<float>();
-	position[1] = this->get_parameter("initial_y").get_value<float>();
-	position[2] = this->get_parameter("initial_theta").get_value<float>();
-	robotControlDataStructure->robot_control_data_access.lock();
-	printRobotLocation = robotControlDataStructure->printRobotLocation;
-	if (robotControlDataStructure->setOdometryPosition) setPosition(robotControlDataStructure->x, robotControlDataStructure->y, robotControlDataStructure->theta);
-	robotControlDataStructure->setOdometryPosition = false;
-	robotControlDataStructure->robot_control_data_access.unlock();
-	if (printRobotLocation) printLocation();
+	x = this->get_parameter("initial_x").get_value<float>();
+	y = this->get_parameter("initial_y").get_value<float>();
+	angle = this->get_parameter("initial_theta").get_value<float>();
 
 	// initialize other parameters
-	leftSpeed = 0;
-	previousLeftSpeed = 0;
-	rightSpeed = 0;
-	previousRightSpeed = 0;
-	acceleration = 0;
-	printRobotLocation = false;
+	speed_l = 0;
+	setSpeed_l = 0;
+	previousSetSpeed_l = 0;
+	speed_r = 0;
+	setSpeed_r = 0;
+	previousSetSpeed_r = 0;
 
 	// initialize variables for odometry calculation
+	this->declare_parameter("motors_acceleration", rclcpp::ParameterValue(1500.0));
+	acceleration = this->get_parameter("motors_acceleration").get_value<float>();
 	this->declare_parameter("wheel_distance", rclcpp::ParameterValue(12.7));
 	this->declare_parameter("e_b", rclcpp::ParameterValue(1.0));
 	wheelDistance = this->get_parameter("e_b").get_value<float>()*this->get_parameter("wheel_distance").get_value<float>(); // m
@@ -35,65 +31,111 @@ OdometryCalculatorNode::OdometryCalculatorNode(motor_data& motorStructure, robot
 	this->declare_parameter("e_d", rclcpp::ParameterValue(1.0));
 	wheelRadius_l = this->get_parameter("wheel_radius").get_value<float>()*2/(this->get_parameter("e_d").get_value<float>()+1); // m
 	wheelRadius_r = this->get_parameter("wheel_radius").get_value<float>()*2/(1/this->get_parameter("e_d").get_value<float>()+1); // m
+	currentTime = this->get_clock()->now().nanoseconds();
+	lastMessageTime = currentTime;
+	accelerationTime_l = 0;
+	accelerationTime_r = 0;
+	distance_l = 0;
+	distance_r = 0;
+	phaseDuration = 0;
 
-	RCLCPP_INFO(this->get_logger(), "%f\t%f\t%f", wheelDistance, wheelRadius_l, wheelRadius_r);
+	// RCLCPP_INFO(this->get_logger(), "%f\t%f\t%f", wheelDistance, wheelRadius_l, wheelRadius_r);
 
-	this->declare_parameter("period", rclcpp::ParameterValue(10));
+	this->declare_parameter("period", rclcpp::ParameterValue(1));
 	period = (float)this->get_parameter("period").get_value<int>()/1000; // s
+	motors_controller_subscriber = this->create_subscription<minirys_interfaces::msg::MotorsControllerOutput>("motors_controller_data", 10, std::bind(&OdometryCalculatorNode::receiveCurrentSetSpeeds, this, std::placeholders::_1));
+	odometry_publisher = this->create_publisher<nav_msgs::msg::Odometry>("odometry_data", 10);
+	msg = nav_msgs::msg::Odometry();
 	odometry_calculation_timer = this->create_wall_timer(
-	std::chrono::milliseconds(this->get_parameter("period").get_value<int>()), std::bind(&OdometryCalculatorNode::calculatePosition, this));
+	std::chrono::milliseconds(this->get_parameter("period").get_value<int>()), std::bind(&OdometryCalculatorNode::updatePosition, this));
 	RCLCPP_INFO(this->get_logger(), "Odometry calculator initialized.");
 }
 
-void OdometryCalculatorNode::calculatePosition() {
-	counter.count();
+OdometryCalculatorNode::~OdometryCalculatorNode() {
+	delete counter;
+}
 
-	previousLeftSpeed = leftSpeed;
-	previousRightSpeed = rightSpeed;
-	motorDataStructure->motor_data_access.lock();
-	leftSpeed = motorDataStructure->leftSpeed; // step/s
-	rightSpeed = motorDataStructure->rightSpeed; // step/s
-	acceleration = motorDataStructure->controller_acceleration*100; // step/s2: step/s/period = step/s/10ms = step/0.01s2 = 100*step/s2
-	motorDataStructure->motor_data_access.unlock();
+void OdometryCalculatorNode::updatePosition() {
+	counter->count();
+	currentTime = this->get_clock()->now().nanoseconds();
+	distance_l = 0;
+	distance_r = 0;
+	accelerationTime_l = 0;
+	accelerationTime_r = 0;
 
-	float accTime_l = abs(leftSpeed - previousLeftSpeed)/acceleration; // s
-	float accTime_r = abs(rightSpeed - previousRightSpeed)/acceleration; // s
-	float D_l = ((leftSpeed + previousLeftSpeed)/2*accTime_l + leftSpeed*(period - accTime_l))/200*2*M_PI*wheelRadius_l; // m: step/(step/circumference)*(m/circumference)
-	float D_r = ((rightSpeed + previousRightSpeed)/2*accTime_r + rightSpeed*(period - accTime_r))/200*2*M_PI*wheelRadius_r; // m: step/(step/circumference)*(m/circumference)
-	float D = (D_l + D_r)/2; // m
-	float changeofAngle = (D_r - D_l)/wheelDistance;
-	position[2] += changeofAngle;
+	phaseDuration = period - (currentTime - lastMessageTime)/1000000000; //time from previous run to new message, if negative there was no new message since last run
+	if (phaseDuration > 0) { //new message from controller was obtained, calculate position at that time
+		if (previousSetSpeed_l != speed_l) { //acceleration left
+			accelerationTime_l = std::min(abs(previousSetSpeed_l - speed_l)/acceleration, phaseDuration);
+			distance_l = (speed_l*accelerationTime_l + (previousSetSpeed_l > speed_l ? 1 : -1)*acceleration*pow(accelerationTime_l, 2)/2)*STEPS_TO_REVS*wheelRadius_l;
+			speed_l = speed_l + accelerationTime_l*acceleration*(previousSetSpeed_l > speed_l ? 1 : -1);
+		}
+		if (previousSetSpeed_r != speed_r) { // acceleration right
+			accelerationTime_r = std::min(abs(previousSetSpeed_r - speed_r)/acceleration, phaseDuration);
+			distance_r = (speed_r*accelerationTime_r + (previousSetSpeed_r > speed_l ? 1 : -1)*acceleration*pow(accelerationTime_r, 2)/2)*STEPS_TO_REVS*wheelRadius_r;
+			speed_r = speed_r + accelerationTime_r*acceleration*(previousSetSpeed_r > speed_l ? 1 : -1);
+		}
+		// driving straight forward
+		distance_l += speed_l*(phaseDuration - accelerationTime_l)*STEPS_TO_REVS*wheelRadius_l;
+		distance_r += speed_r*(phaseDuration - accelerationTime_r)*STEPS_TO_REVS*wheelRadius_r;
+	}
+
+	phaseDuration = period - (phaseDuration > 0 ? phaseDuration : 0); // time from last message to now or this functions period, whichevers lower
+	if (setSpeed_l != speed_l) { //acceleration left
+		accelerationTime_l = std::min(abs(setSpeed_l - speed_l)/acceleration, phaseDuration);
+		distance_l = (speed_l*accelerationTime_l + (setSpeed_l > speed_l ? 1 : -1)*acceleration*pow(accelerationTime_l, 2)/2)*STEPS_TO_REVS*wheelRadius_l;
+		speed_l = speed_l + accelerationTime_l*acceleration*(setSpeed_l > speed_l ? 1 : -1);
+	}
+	if (setSpeed_r != speed_r) { // acceleration right
+		accelerationTime_r = std::min(abs(setSpeed_r - speed_r)/acceleration, phaseDuration);
+		distance_r = (speed_r*accelerationTime_r + (setSpeed_r > speed_l ? 1 : -1)*acceleration*pow(accelerationTime_r, 2)/2)*STEPS_TO_REVS*wheelRadius_r;
+		speed_r = speed_r + accelerationTime_r*acceleration*(setSpeed_r > speed_l ? 1 : -1);
+	}
+	// driving straight forward
+	distance_l += speed_l*(phaseDuration - accelerationTime_l)*STEPS_TO_REVS*wheelRadius_l;
+	distance_r += speed_r*(phaseDuration - accelerationTime_r)*STEPS_TO_REVS*wheelRadius_r;
+
+	angle += (distance_r - distance_l)/wheelDistance;
 	cropAngle();
-	position[0] = position[0] + D*cos(position[2]);
-	position[1] = position[1] + D*sin(position[2]);
+	x = x + (distance_l+distance_r)/2*cos(angle);
+	y = y + (distance_l+distance_r)/2*sin(angle);
 
-	robotControlDataStructure->robot_control_data_access.lock();
-	printRobotLocation = robotControlDataStructure->printRobotLocation;
-	robotControlDataStructure->printRobotLocation = false;
-	if (robotControlDataStructure->setOdometryPosition) setPosition(robotControlDataStructure->x, robotControlDataStructure->y, robotControlDataStructure->theta);
-	robotControlDataStructure->setOdometryPosition = false;
-	robotControlDataStructure->robot_control_data_access.unlock();
+	msg.header.stamp = this->get_clock()->now();
+	msg.pose.pose.position.x = x;
+	msg.pose.pose.position.y = y;
+	msg.pose.pose.orientation.w = cos(angle/2);
+	msg.pose.pose.orientation.x = 0;
+	msg.pose.pose.orientation.y = 0;
+	msg.pose.pose.orientation.z = sin(angle/2);
+	odometry_publisher->publish(msg);
 
-	odometryDataStructure->odometry_data_access.lock();
-	odometryDataStructure->x = position[0];
-	odometryDataStructure->y = position[1];
-	odometryDataStructure->theta = position[2];
-	odometryDataStructure->odometry_data_access.unlock();
+	printLocation();
+}
 
-	if (printRobotLocation) printLocation();
+void OdometryCalculatorNode::receiveCurrentSetSpeeds(const minirys_interfaces::msg::MotorsControllerOutput::SharedPtr msg) {
+	previousSetSpeed_l = setSpeed_l;
+	previousSetSpeed_r = setSpeed_r;
+	lastMessageTime = msg->header.stamp.sec*1000000000 + msg->header.stamp.nanosec;
+	if (setSpeed_l != msg->left_wheel_speed){
+		setSpeed_l = msg->left_wheel_speed;
+	}
+	if (setSpeed_r != msg->right_wheel_speed){
+		setSpeed_r = msg->right_wheel_speed;
+	}
+	RCLCPP_INFO(this->get_logger(), "%f\t%f", speed_l, speed_r);
 }
 
 void OdometryCalculatorNode::printLocation() {
-	RCLCPP_INFO(this->get_logger(), "Robot location:\n\tx: %.4f\n\ty: %.4f\n\tO: %.4f", position[0], position[1], position[2]);
+	RCLCPP_INFO(this->get_logger(), "Robot location:\n\tx: %.4f\n\ty: %.4f\n\tO: %.4f", x, y, angle);
 }
 
 void OdometryCalculatorNode::cropAngle() {
-	if (position[2] > M_PI) position[2] -= 2*M_PI;
-	if (position[2] < -M_PI) position[2] += 2*M_PI;
+	if (angle > M_PI) angle -= 2*M_PI;
+	if (angle < -M_PI) angle += 2*M_PI;
 }
 
 void OdometryCalculatorNode::setPosition(float ix, float iy, float itheta) {
-	position[0] = ix;
-	position[1] = iy;
-	position[2] = itheta;
+	x = ix;
+	y = iy;
+	angle = itheta;
 }
